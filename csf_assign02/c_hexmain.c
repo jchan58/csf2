d302bbfaf665c5e9e7b516570abafb03850866cc
Gbemisola Oladosu <golados1@jhu.edu>
2022-02-18 15:13:56 -0500

    main work on

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -3,39 +3,41 @@
 #include "hexfuncs.h"  // this is the only header file which may be included!
 
 int main(int argc, char *argv[]){
-  /*
   //read the input file
-  int count = 0;
+  int line_count = 0;
   char line[16];
   char hex_sentence[16];
   char hex_printable[16];
   //char hex_formatted_address[dont know the size];
   //i think this is how it's done
-  while(line = hex_read(argv[0]), line != 0){
+  while((line_count = hex_read(line)) != 0){
     //hex_format_offset the address 
     //hex_write_string(write the formatted address column, and write a space
     hex_write_string(":  ");
 
-    for(int i = 0; i < line; i++){
+    //make sure line is null terminated
+    line[line_count] = '\0';
+    
+    for(int i = 0; i < line_count; i++){
       //convert the sentence into a hex sentence
-      hex_sentence[i] =  hex_format_byte_as_hex(line[i]);
+      hex_format_byte_as_hex(line[i], hex_sentence);
     }
+    
     //write the sentence segment in hexadecimal
     hex_write_string(hex_sentence);
     hex_write_string("  ");
 
     //make sure all the sentence values are printable
-    for(int i = 0; i < line; i++){
+    for(int i = 0; i < line_count; i++){
       //convert the sentence into a hex sentence
-      hex_printable[i] =  hex_to_printable(line[i]);
+      hex_printable[i] = hex_to_printable(line[i]);
     }
 
     //print a line 
-    hex_write_string(hex_printable[i]);
+    hex_write_string(hex_printable);
 
     //write a newline
     hex_write_string("\n");
-
-  */
+  
 }
 
