a659efe0ec0acf3bd7b205c687e5464090bf6184
Gbemisola Oladosu <golados1@jhu.edu>
2022-03-07 23:16:50 -0500

    worked on main

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -29,6 +29,26 @@ int isPowerOfTwo(long num){
 
 int main(int argc, char* argv[]){
 
+  typedef struct Slot {
+    //store the tag of the slot
+    unsigned tag;
+
+    //store the index of the slot
+    unsigned index; 
+
+    //store the offset of the slot
+    unsigned offset;
+
+    //store the load timestamp of the slot
+    unsigned load_stamp;
+
+    //store the access timestamp of the slot
+    unsigned access_stamp;
+
+
+  } Slot;
+
+  //order vector based off of load stamp or access stamp, depending on eviction type!
 
   //argv[1] is number of sets in cache, pos power of 2
   //argv[2] is number of blocks in set, pos power of 2
@@ -36,26 +56,50 @@ int main(int argc, char* argv[]){
   //argv[4] is write-allocate or no-write -allocate
   //argv[5] is write-through or write-back
   //argv[6] is lru or fifo evictions
-  
+  //think about what else could go wrong with arguments (remove above later)
+
   if(argc < 7){
      fprintf(stderr, "Must enter all six arguments.\n");
      return 1;
   }
 
-  //think about what else could go wrong with arguments
   
+   /*these don't work for some reason
+   //check if argv[4] valid"
+   if((strcmp(argv[4], "write-allocate") != 0) || (strcmp(argv[4], "no-write-allocate") != 0)){
+     fprintf(stderr, "Cache miss paramters must be write-allocate or no-write allocate");
+     return 1;
+   }
+
+    //these don't work for some reason
+   //check if argv[5] is "write-through" or "write-back"
+   if((strcmp(argv[5], "write-through") != 0) || (strcmp(argv[6], "write-back") != 0)){
+     fprintf(stderr, "Write type must be write-back or write-through");
+     return 1;
+   }
+
+   //check if argv[6] is "lru" or "fifo"
+   if((strcmp(argv[6], "lru") != 0) || (strcmp(argv[6], "fifo") != 0)){
+     fprintf(stderr, "Evictions do not match lru or fifo.\n");
+     return 1;
+   }
+   */
+
+
+
+  //convert the args representing integers into integers
   long setNum = strtol(argv[1], NULL, 10);
   long blockNum = strtol(argv[2], NULL, 10);
   long bytesPerBlock = strtol(argv[3], NULL, 10);
 
  
-  
+  //these params cant coexist
   if(strcmp(argv[4], "no-write-allocate") == 0 && strcmp(argv[5], "write-back") == 0){ 
     fprintf(stderr, "Cannot be no-write allocate and write back.\n");
     return 1;
   }
 
-  
+  //make sure integer values are with range
   if(setNum < 0 || blockNum < 0){
     fprintf(stderr, "Set and block number must be positive.\n");
     return 1;
@@ -67,7 +111,7 @@ int main(int argc, char* argv[]){
     return 1;
   }
 
-
+   //make sure integer values are powers of 2
    if(isPowerOfTwo(setNum) != 1){
      fprintf(stderr, "Number of sets should be a power of two.\n");
      return 1;
@@ -83,11 +127,12 @@ int main(int argc, char* argv[]){
      return 1;
    }
 
-   //check if argv[6] is "lru" or "fifo"
-    if(strcmp(argv[6], "lru") == 0 || strcmp(argv[6], "fifo") == 0){
-    fprintf(stderr, "Evictions do not match lru or fifo.\n");
-    return 1;
-  }
+   
+    
+   
+
+
+   
    
    /*started writing read from standard in
    char* trace_line = NULL;
