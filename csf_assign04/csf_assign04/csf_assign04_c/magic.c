dba12306358f3cccf7a6d66e29afd5ca761ebd6a
jchan58 <jchan58@jh.edu>
2022-04-01 16:05:56 -0400

    added stuff for assign04

---
+++ csf_assign04/csf_assign04/csf_assign04_c/magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <stdint.h>
+#include <elf.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/unistd.h>
+#include <fcntl.h>
+
+#include "elf_names.h"
+
+int main(int argc, char **argv) {
+  // TODO: implement
+
+  return 0;
+}
