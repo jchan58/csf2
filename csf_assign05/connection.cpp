26e6faa711d2d853fd78626089445e62566a3820
Gbemisola Oladosu <golados1@jhu.edu>
2022-04-15 16:28:41 -0400

    wrote connection.cpp

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -11,42 +11,66 @@ Connection::Connection()
 }
 
 Connection::Connection(int fd)
-  rio_t rio;
   : m_fd(fd)
   , m_last_result(SUCCESS) {
   //call rio_readinitb to initialize the rio_t object
-  rio_readinitb(&rio, fd);
+  rio_readinitb(&m_fdbuf, fd);
 }
 
 void Connection::connect(const std::string &hostname, int port) {
-  rio_t rio;
   //open_clientfd to connect to the server
-  int ready_fd = open_clientfd(hostname, (char *) &port);
+  int ready_fd = open_clientfd(hostname, &((char) port));
   //call rio_readinitb to initialize the rio_t object
-  rio_readinitb(&rio, ready_fd);
+  rio_readinitb(&m_fdbuf, ready_fd);
 }
 
 Connection::~Connection() {
-  // TODO: close the socket if it is open
-  //how to get socket fd?
+  //close the socket if it is open
+  close(m_fd);
 }
 
 bool Connection::is_open() const {
   // TODO: return true if the connection is open
+  return m_fd >= 0;
 }
 
 void Connection::close() {
   // TODO: close the connection if it is open
+  if(m_fd >= 0) {
+    //how is this different than closing the socket?
+    close(m_fd);
+  }
 }
 
 bool Connection::send(const Message &msg) {
   // TODO: send a message
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
+  
+  //send to server fd, message
+  ssize_t result = rio_writen(m_fd, msg, strlen(*msg));
+  m_last_result = result;
+  if(result < 0){
+    //success
+    return false;
+  } else {
+    return true;
+  }
+  
 }
 
 bool Connection::receive(Message &msg) {
   // TODO: send a message, storing its tag and data in msg
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
+
+  //read into message from the server fd
+  ssize_t result = rio_readlineb(msg, msg->MAX_LEN, sizeof(buf));
+  m_last_result = result;
+  if(result < 0){
+    //success
+    return false;
+  } else {
+    return true;
+  }
 }
